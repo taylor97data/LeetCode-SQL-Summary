e227541e0a1fa0e96b2f26991f1c13cf06a36119
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-08 20:35:48 -0400

    Create 1322. 广告效果.sql

---
+++ case-when/1322. 广告效果.sql
@@ -0,0 +1,5 @@
+# Write your MySQL query statement below
+SELECT ad_id, CASE when sum(if(action='Clicked',1,0))+sum(if(action='Viewed',1,0))=0 then 0
+                    else round(100*sum(if(action='Clicked',1,0))/(sum(if(action='Clicked',1,0))+sum(if(action='Viewed',1,0))),2) end as ctr FROM ads
+GROUP BY ad_id
+ORDER BY ctr desc,ad_id;
