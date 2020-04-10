415256111bf8e0f6d8fef653252f45c940aecab2
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-10 11:49:11 -0400

    Create 626. 换座位.sql

---
+++ case-when/626. 换座位.sql
@@ -0,0 +1,7 @@
+SELECT (CASE 
+            WHEN MOD(id,2) = 1 AND id = (SELECT COUNT(*) FROM seat) THEN id
+            WHEN MOD(id,2) = 1 THEN id + 1
+            ElSE id-1
+        END) AS id, student
+FROM seat
+ORDER BY id;
