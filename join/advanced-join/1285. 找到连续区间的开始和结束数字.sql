fc5fac97400aee5d7a598aeeabd4fa0577b32a36
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-12 23:50:47 -0400

    Update 1285. 找到连续区间的开始和结束数字.sql

--- join/advanced-join/1285. 找到连续区间的开始和结束数字.sql
+++ join/advanced-join/1285. 找到连续区间的开始和结束数字.sql
@@ -5,3 +5,14 @@ join (
 SELECT log_id from logs where log_id+1 not in (select log_id from logs)) b
 on a.log_id <= b.log_id
 group by a.log_id
+
+#方法二 定义变量
+                                               
+SELECT min(log_id) as start_id,max(log_id) as end_id
+from (
+select log_id, case when @id = log_id - 1 then @num:=@num
+ELSE @num:=@num+1 end as num, @id:= log_id from logs,(SELECT @num:=0, @id:=0) a
+) temp
+group by num
+                                        
+                                               
