c34653357050164ef1233e486e916a7603d84f5f
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-22 20:00:24 +0800

    Update 1070. 产品销售分析 III.sql

--- join/simple_join/1070. 产品销售分析 III.sql
+++ join/simple_join/1070. 产品销售分析 III.sql
@@ -2,3 +2,12 @@ SELECT s.product_id,year as first_year,quantity,price
 FROM sales s
 WHERE (product_id,year) in (
 SELECT product_id, min(year) FROM sales GROUP BY product_id)
+
+
+
+SELECT s.product_id,first_year,quantity,price
+FROM sales s
+INNER JOIN
+(SELECT product_id, min(year) as first_year FROM sales
+GROUP BY product_id) t
+ON s.product_id = t.product_id and s.year = t.first_year
