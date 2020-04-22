b9f8b1905c57c2c3b493cd65c7d494a04580102f
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-23 00:14:18 +0800

    Create 1132. 报告的记录 II.sql

---
+++ join/simple_join/1132. 报告的记录 II.sql
@@ -0,0 +1,7 @@
+SELECT ROUND(AVG(proportion)*100,2) AS average_daily_percent FROM(
+SELECT actions.action_date, COUNT(DISTINCT removals.post_id)/COUNT(DISTINCT actions.post_id) as proportion
+FROM actions
+LEFT JOIN removals
+ON actions.post_id = removals.post_id
+WHERE extra = 'spam'
+GROUP BY actions.action_date) a
