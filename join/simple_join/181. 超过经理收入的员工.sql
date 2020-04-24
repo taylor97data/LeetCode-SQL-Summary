18c56319f4bd9e44b401e6f275ccc37698297709
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-24 13:49:58 +0800

    Create 181. 超过经理收入的员工.sql

---
+++ join/simple_join/181. 超过经理收入的员工.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+SELECT e1.name as Employee
+FROM employee e1
+INNER JOIN employee e2
+ON e1.managerid = e2.id
+WHERE e1.managerid IS NOT NULL
+AND e1.salary > e2.salary
+
