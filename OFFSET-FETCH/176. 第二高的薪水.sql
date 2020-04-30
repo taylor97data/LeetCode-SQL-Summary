6642f8780a098d009a0398cd6b8dd3b32f764be3
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-30 09:13:32 +0800

    Create 176. 第二高的薪水.sql

---
+++ OFFSET-FETCH/176. 第二高的薪水.sql
@@ -0,0 +1,11 @@
+SELECT max(salary)AS SecondHighestSalary FROM Employee 
+WHERE salary < (SELECT MAX(salary) FROM Employee);
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
