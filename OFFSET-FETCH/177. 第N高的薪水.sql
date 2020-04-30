fafdf43ba1f330afd8fe261efeaf1bcc938cdf90
taylor97data <56550882+taylor97data@users.noreply.github.com>
2020-04-30 10:39:53 +0800

    Create 177. 第N高的薪水.sql

---
+++ OFFSET-FETCH/177. 第N高的薪水.sql
@@ -0,0 +1,9 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+    DECLARE P INT;
+    SET P = N-1;
+  RETURN (
+      (SELECT DISTINCT Salary FROM Employee ORDER BY Salary DESC LIMIT P,1)
+      
+  );
+END
